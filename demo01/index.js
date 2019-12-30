50dbe5fe77b4455475aecc7e2c1720f7c1875921
wangchunyun <m18290107861@163.com>
2019-12-30 17:14:30 +0800

    提交代码

--- demo01/index.js
+++ demo01/index.js
@@ -1,9 +1,11 @@
-const rabbirS = require('../rabbitmq/lib/rabbitmqServer');
-const rabbirC = require('../rabbitmq/lib/rabbitmqClient');
+const rabbirS = require('./lib/rabbitmqServer');
+const rabbirC = require('./lib/rabbitmqClient');
 
 (async () => {
+    // 生成一个消息
     await rabbirS.rabbitmqServer('收到后傻傻的开会了');
 
+    // 消费掉消息
     setTimeout(async function () {
         await rabbirC.rabbitmqClient()
     }, 1000)
