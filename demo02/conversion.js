1c5eca60e4c1567dac771d9408886b19fc8f539e
wangchunyun <m18290107861@163.com>
2020-02-26 13:49:26 +0800

    fix

--- demo02/conversion.js
+++ demo02/conversion.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
-
+const _l = require('loadsh');
 
 module.exports.conver = (dir) => {
     const fileContent = fs.readFileSync(dir, 'hex');
+
+    let arr1 = [
+        3113829,3114247,3114269,3114273,3118598,3118727,3118729
+    ];
+    let arr2 = [
+        3113829,3114247,3114269,3114273,3114285,3118727,3118729
+    ];
+
+    
+    console.log(_l.difference(arr1, arr2));
+    
     return fileContent;
 }
